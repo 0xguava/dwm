84133e2a07fea2235fb0250d33f91cefbc41d0d3
0xguava <demo.lappy@protonmail.com>
2023-04-20 12:46:52 +0530

    gaps set

--- config.def.h
+++ config.def.h
@@ -2,7 +2,7 @@
 
 /* appearance */
 static const unsigned int borderpx  = 2;        /* border pixel of windows */
-static const unsigned int gappx     = 5;        /* gaps between windows */
+static const unsigned int gappx     = 7;        /* gaps between windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -21,7 +21,7 @@ static const unsigned int alphas[][3]	={
 };
 
 /* tagging */
-static const char *tags[] = { "", "","", "", "󰇮", "", "", "", "" };
+static const char *tags[] = { "󰍳", "","", "", "󰇮", "", "", "", "" };
 
 static const Rule rules[] = {
 	/* xprop(1):
